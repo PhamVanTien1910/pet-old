e6555d1a7af9f7cf6c1e631897f294024ef121fa
PhamVanTien <tienphamvan2005@gmail.com>
2024-08-11 00:41:18 +0700

    PC-06: Get all váºÃeterinarian

---
+++ src/main/java/com/tienpv/petcare/infrastructure/converter/IVeterinarianConverter.java
@@ -0,0 +1,8 @@
+package com.tienpv.petcare.infrastructure.converter;
+
+import com.tienpv.petcare.application.dto.response.VeterinarianResponse;
+import com.tienpv.petcare.domain.entity.VeterinarianEnity;
+
+public interface IVeterinarianConverter {
+    VeterinarianResponse toDTO(VeterinarianEnity enity);
+}
